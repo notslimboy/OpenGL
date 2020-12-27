6e3225c7d8b48d4b2eefaac5ba72e3117832e434
Raka <raka.arya34@gmail.com>
2020-12-27 15:33:10 +0700

    Finish Tutorial 19 and 20

--- OpenGL x86/OpenGL X86/main.cpp
+++ OpenGL x86/OpenGL X86/main.cpp
@@ -9,11 +9,11 @@
 
 Vertex Vertices[] = 
 {
-	// positions						// colors						// texcoords
-	glm::vec3(-0.5f, 0.5f, 0.f),		glm::vec3(1.f, 0.f, 0.f),		glm::vec2(0.f, 1.f),
-	glm::vec3(-0.5f, -0.5f, 0.f),		glm::vec3(0.f, 1.f, 0.f),		glm::vec2(0.f, 0.f),
-	glm::vec3(0.5f, -0.5f, 0.f),		glm::vec3(0.f, 0.f, 1.f),		glm::vec2(1.f, 0.f),
-	glm::vec3(0.5f, 0.5f, 0.f),			glm::vec3(1.f, 1.f, 0.f),		glm::vec2(1.f, 1.f)
+	// positions						// colors						// texcoords				// normal
+	glm::vec3(-0.5f, 0.5f, 0.f),		glm::vec3(1.f, 0.f, 0.f),		glm::vec2(0.f, 1.f),		glm::vec3(0.f, 0.f, -1.f),
+	glm::vec3(-0.5f, -0.5f, 0.f),		glm::vec3(0.f, 1.f, 0.f),		glm::vec2(0.f, 0.f),		glm::vec3(0.f, 0.f, -1.f),
+	glm::vec3(0.5f, -0.5f, 0.f),		glm::vec3(0.f, 0.f, 1.f),		glm::vec2(1.f, 0.f),		glm::vec3(0.f, 0.f, -1.f),
+	glm::vec3(0.5f, 0.5f, 0.f),			glm::vec3(1.f, 1.f, 0.f),		glm::vec2(1.f, 1.f), 		glm::vec3(0.f, 0.f, -1.f)
 };
 unsigned nrOfVertices = sizeof(Vertices) / sizeof(Vertex);
 
@@ -253,6 +253,10 @@ int main()
 	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, texcoord));
 	glEnableVertexAttribArray(2);
 
+	// normal
+	glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, normal));
+	glEnableVertexAttribArray(3);
+
 	// bind VAO
 	glBindVertexArray(0);
 
@@ -342,12 +346,16 @@ int main()
 	ProjectionMatrix = glm::perspective(glm::radians(fov), static_cast<float>(framebufferWidth) / framebufferHeight, nearPlane, farPlane);
 
 
-	glUseProgram(core_program);
+	// lights
+	glm::vec3 lightPos0(0.f, 0.f, 1.f);
 
 	// init uniform
+	glUseProgram(core_program);
 	glUniformMatrix4fv(glGetUniformLocation(core_program, "ModelMatrix"), 1, GL_FALSE, glm::value_ptr(ModelMatrix));
 	glUniformMatrix4fv(glGetUniformLocation(core_program, "ViewMatrix"), 1, GL_FALSE, glm::value_ptr(ViewMatrix));
 	glUniformMatrix4fv(glGetUniformLocation(core_program, "ProjectionMatrix"), 1, GL_FALSE, glm::value_ptr(ProjectionMatrix));
+	glUniform3fv(glGetUniformLocation(core_program, "lightPos0"), 1, glm::value_ptr(lightPos0));
+	glUniform3fv(glGetUniformLocation(core_program, "cameraPos"), 1, glm::value_ptr(camPosition));
 
 	glUseProgram(0);
 
@@ -358,7 +366,7 @@ int main()
 		glfwPollEvents();
 		updateInput(window, position, rotation, scale);
 
-		// update
+		// update	
 		updateInput(window);
 
 		// clear
